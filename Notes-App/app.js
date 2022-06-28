4883c252976d50988fb5e05e7f08b435d3b445ff
DeepakGitHub050 <ds1008076@gmail.com>
2022-06-28 17:06:23 +0530

    Chalk & nodemon module added

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,5 +1,15 @@
-const name = require("./log.js");
+/*const name = require("./log.js");
 
 require("./log.js");
 console.log("This is app js");
 console.log(name.lname);
+*/
+
+/*const valid = require("validator");
+
+console.log(valid.isEmail("adgdfs@afdf.com"));
+*/
+
+const chalk = require("chalk");
+//const Chal = require("chalk");
+console.log(chalk.green.bold("Success!"));

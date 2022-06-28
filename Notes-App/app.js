065486bc359ee7c188ee484c94094a1e210df141
DeepakGitHub050 <ds1008076@gmail.com>
2022-06-28 16:15:15 +0530

    npm package initialized

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,2 +1,5 @@
-const fs = require("fs");
-fs.writeFileSync("new.txt", "This file is created using Node Js");
+const name = require("./log.js");
+
+require("./log.js");
+console.log("This is app js");
+console.log(name.lname);
---
+++ Notes-App/log.js
@@ -0,0 +1,5 @@
+console.log("This is Log Js file ");
+
+const name = "DKS";
+const lname = "Sharma";
+module.exports = { name, lname };

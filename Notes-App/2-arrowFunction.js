cff86f02debc461dbc4e81e3a8a356629a99b22c
DeepakGitHub050 <ds1008076@gmail.com>
2022-06-29 18:38:45 +0530

    Notes app completed

--- Notes-App/2-arrowFunction.js
+++ Notes-App/2-arrowFunction.js
@@ -44,6 +44,13 @@ const listNotes = () => {
 
 const readNotes = (title) => {
   const notes = loadNotes();
+  const note = notes.find((note) => note.title == title);
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found"));
+  }
 };
 
 const saveNotes = (notes) => {
